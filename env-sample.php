7832fb77cd3fbbd6493aa025fd1de719257d5575
Lucas Buzzo <lcsbzo@gmail.com>
2017-12-24 16:03:16 -0200

    Added rpc-api

--- env-sample.php
+++ env-sample.php
@@ -1,7 +1,16 @@
 <?php
 
+##MySQL Vars
 $driver = 'mysql';
 $host = 'localhost';
 $db = 'voting';
 $user = 'chaincoin';
 $pass = 'chainPass';
+
+#RPC-API Vars
+$settings = array(
+  'rpc.host' => changeMe.host,
+  'rpc.port' => changeMe.port,
+  'rpc.user' => changeMe.user,
+  'rpc.pass' => changeMe.pass
+);
